5dec8341869d85cdf27f32d3f3eb7da5b673c6ed
Viktor Asenov <viktorasenov006@gmail.com>
2023-12-11 13:04:25 +0200

    everything until 9.12

--- examples/flight-diary/src/services/diaryService.ts
+++ examples/flight-diary/src/services/diaryService.ts
@@ -1,26 +1,39 @@
 import diaries from '../../data/diaries';
 
-import { NonSensitiveDiaryEntry, DiaryEntry } from '../types';
+import { NewDiaryEntry, NonSensitiveDiaryEntry, DiaryEntry } from '../types';
 
 const getEntries = (): DiaryEntry[] => {
   return diaries;
 };
 
 const getNonSensitiveEntries = (): NonSensitiveDiaryEntry[] => {
-  return diaries.map(({ id, date, weather, visibility}) => ({
+  return diaries.map(({ id, date, weather, visibility }) => ({
     id,
     date,
     weather,
-    visibility
+    visibility,
   }));
 };
 
-const addDiary = () => {
-  return null;
+const addDiary = (entry: NewDiaryEntry): DiaryEntry => {
+  const newDiaryEntry = {
+    id: Math.max(...diaries.map(d => d.id)) + 1,
+    ...entry
+  };
+
+  diaries.push(newDiaryEntry);
+
+  return newDiaryEntry;
+};
+
+const findById = (id: number): DiaryEntry | undefined => {
+  const entry = diaries.find(d => d.id === id);
+  return entry;
 };
 
 export default {
   getEntries,
   getNonSensitiveEntries,
-  addDiary
+  addDiary,
+  findById
 };
--- part9/patientor-backend/src/index.ts
+++ part9/patientor-backend/src/index.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import diagnosisRouter from './routers/diagnoses';
+import patientRouter from './routers/patients';
 import cors from 'cors';
 const app = express();
 app.use(express.json());
@@ -10,6 +12,9 @@ app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
+
+app.use('/api/diagnoses', diagnosisRouter);
+app.use('/api/patients', patientRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
